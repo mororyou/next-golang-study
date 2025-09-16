483510b18cbeaf8da3a73af8bf60f7be81663a37
R.Moroizumi <m.ryousuke0401@iclioud.com>
2025-09-17 00:18:54 +0900

    🐛 query bug fix

--- services/goApi/repository/task_repository.go
+++ services/goApi/repository/task_repository.go
@@ -32,7 +32,7 @@ func (tr *taskRepository) GetAllTasks(tasks *[]model.Task, userId uint) error {
 }
 
 func (tr *taskRepository) GetTaskById(task *model.Task, userId uint, taskId uint) error {
-	if err := tr.db.Joins("User").Where("id = ?", userId).First(task, taskId).Error; err != nil {
+	if err := tr.db.Joins("User").Where("user_id = ?", userId).First(task, taskId).Error; err != nil {
 		return err
 	}
 	return nil
